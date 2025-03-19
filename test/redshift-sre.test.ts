e61eab67aba1de0fb323f8e7b58da8a7913f6a23
Andrew Achtenberg <aachten@gmail.com>
2025-03-19 10:49:53 -0400

    initiated cdk project

---
+++ test/redshift-sre.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RedshiftSre from '../lib/redshift-sre-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/redshift-sre-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RedshiftSre.RedshiftSreStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
